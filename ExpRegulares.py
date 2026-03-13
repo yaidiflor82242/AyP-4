aa197502d5adc5eef887b81768b52a70164c49e0
Yaidi <yaidi_florez82242@elpoli.edu.co>
2026-03-12 20:41:23 -0500

    ejemplos expresiones y conjuutnos

---
+++ ExpRegulares.py
@@ -0,0 +1,28 @@
+import re
+texto= "python hola, es genial"
+
+resultado=re.match("python",texto)
+print (resultado)
+
+
+texto= "python hola, es genial"
+
+resultado=re.search("python",texto)
+print (resultado.start())
+
+texto= "python hola, es genial"
+
+resultado=re.findall("python",texto)
+print ("encontrado" ,resultado)
+
+
+texto= "python hola, es genial"
+
+resultado=re.findall("python",texto, re.IGNORECASE)
+print (resultado)
+
+
+texto1= "terngo 3 manzanas,12 naranjas y 100 uvas"
+
+resultado=re.findall(r"\d+",texto1)
+print ("encontrado", resultado)
